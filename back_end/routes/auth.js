4bac4237b9016ead09465b519d54ee111aae7207
Issam Aloui <mrdivanodz@gmail.com>
2025-04-11 23:26:17 +0100

    bringing back the routes folder contents

---
+++ back_end/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { signup, login, logout } = require("../controllers/authController");
+const { validateSignup, validateLogin } = require("../middleware/authchecker");
+const { signupLimiter, loginLimiter } = require("../middleware/Security");
+
+const router = express.Router();
+
+router.post("/signup", signupLimiter, validateSignup, signup);
+router.post("/login", loginLimiter, validateLogin, login);
+router.post("/logout", logout);
+
+module.exports = router;

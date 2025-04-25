fbf3b51a3e57ced928e503afb0b5a4ce6a7df60d
dadi abderrahim <dadiabdrhm@gmail.com>
2025-04-25 15:51:54 +0100

    Add admin verification to announcement routes and fix typo in error message(the fealds turned into fialds)

--- back_end/middleware/Security.js
+++ back_end/middleware/Security.js
@@ -63,4 +63,10 @@ exports.verifyJWT = (req, res, next) => {
     next();
   });
 };
-
+exports.verifyAdmin = (req, res, next) => {
+  const { role } = req.user
+  if (role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admins only."})
+  }
+  next()
+}

df67e5349e69bbb7b84ad2ade09c54d36d8823b1
Issam Aloui <mrdivanodz@gmail.com>
2025-05-29 01:01:55 +0100

    Add node-cron for daily and monthly statistics reset functionality

--- back_end/server.js
+++ back_end/server.js
@@ -17,7 +17,8 @@ const path = require("path");
 const servicesr = require("./routes/Reviews");
 const checkin = require("./routes/CheckIn");
 const annoucementsi = require("./routes/Announcement");
-
+const cron = require("node-cron");
+const Statistiques = require("./models/statistiques");
 dotenv.config();
 const app = express();
 const Port = process.env.PORT || 5000;
@@ -36,7 +37,7 @@ app.use("/reviews", servicesr);
 app.use("/user", user1);
 app.use("/gym", gym);
 app.use("/announcements", annoucementsi);
-app.use("/scan",checkin)
+app.use("/scan", checkin);
 app.use("/services", servicesm);
 app.use("/paypal", paypalRoutes);
 app.use("/", serveFront);
@@ -60,7 +61,38 @@ mongoose
   .catch((err) => {
     logger.error("❌ Database connection error:", err);
   });
+cron.schedule("0 0 * * *", async () => {
+  try {
+    await Statistiques.updateMany(
+      {},
+      {
+        $set: {
+          dailyCheckIns: 0,
+          newSignUps: 0,
+        },
+      }
+    );
+    console.log("✅ Daily stats reset");
+  } catch (err) {
+    console.error("❌ Error resetting daily stats:", err.message);
+  }
+});
 
+cron.schedule("0 0 1 * *", async () => {
+  try {
+    await Statistiques.updateMany(
+      {},
+      {
+        $set: {
+          monthlyRevenue: 0,
+        },
+      }
+    );
+    console.log("✅ Monthly revenue reset");
+  } catch (err) {
+    console.error("❌ Error resetting monthly revenue:", err.message);
+  }
+});
 process.on("uncaughtException", (err) => {
   logger.error("Uncaught Exception:", err);
 });
